ea05fa15864021445a2fc2433518cf135a3ec755
anujborde <72304394+anujborde@users.noreply.github.com>
2020-10-21 11:10:31 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,84 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG,b1,b2,b3
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	b1=createSprite(470,620,10,80)
+	b1.shapeColor=color("red")
+
+	b2=createSprite(300,620,10,80)
+	b2.shapeColor=color("red")
+
+	b3=createSprite(385,655,160,10)
+	b3.shapeColor=color("red")
+
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0, isStatic:true});
+	World.add(world, packageBody);
+	
+	
+b3=Bodies.rectangle(b3.position.x,b3.position.y,b3.width,b3.height,{isStatic:true})
+World.add(world,b3)
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+
+
+  Engine.update(engine)
+
+keyPressed();
+
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+    // Look at the hints in the document and understand how to make the package body fall only on
+	Matter.Body.setStatic(packageBody,false);
+	
+  }
+}
+
+
